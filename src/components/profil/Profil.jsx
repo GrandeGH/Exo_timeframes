72b981961bf4eeecd0d364486ab7f4231b5e96dc
GrandeGH <georgesgrande96@gmail.com>
2025-02-26 16:45:58 +0100

    3eme commit

--- src/components/profil/Profil.jsx
+++ src/components/profil/Profil.jsx
@@ -7,27 +7,30 @@ function Profil({setPeriode}) {
             <div className='bg-primary rounded-4'  style={{
               maxHeight:"500px"
             }}>
-                <div className='p-3'>
-                    <div className='imgJeremy my-3'></div>
-                    <div>
+                <div className='row p-3'>
+                    <div className='col-sm-6 col-lg-12'>
+                        <div className='imgJeremy my-3 '></div>
+                    </div>
+                    <div className='col-sm-6 col-lg-12'>
                         <p>Report for</p>
                         <h2 className='mb-5'>Jeremy Robson</h2>    
                     </div>
                 </div>
-
-                <div className='bg-dark p-3 rounded-4' style={{
-              maxHeight:"127px"
-            }}>
-                    <div className='mb-1'>
-                        <button onClick={()=> setPeriode("daily")}>Daily</button>
-                    </div>
-                    <div className='mb-1'>
-                        <button onClick={()=> setPeriode("weekly")}>Weekly</button>
-                    </div>
-                    <div className=''>
-                        <button onClick={()=> setPeriode("monthly")}>Monthly</button>
-                    </div>
+              <div className='container'>                
+                <div className='row bg-dark py-3 rounded-4' style={{
+                  maxHeight:"127px"
+                    }}>       
+                  <div className='col-sm-4 col-md-12'>
+                      <button onClick={()=> setPeriode("daily")}>Daily</button>
+                  </div>  
+                  <div className='col-sm-4 col-md-12'>
+                      <button onClick={()=> setPeriode("weekly")}>Weekly</button>
+                  </div> 
+                  <div className='col-sm-4 col-md-12'>
+                      <button onClick={()=> setPeriode("monthly")}>Monthly</button>
+                  </div> 
                 </div>
+              </div>
             </div>
 
         </>
